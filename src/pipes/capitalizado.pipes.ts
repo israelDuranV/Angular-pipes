b4be3fb84b263767ed2df151cd4da0a9348c2fde
Israel Duran <israel_duran@outlook.com>
2019-09-05 12:58:11 -0500

    Agregando pipes personalizados

---
+++ src/pipes/capitalizado.pipes.ts
@@ -0,0 +1,8 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({name: 'capitalizado'})
+export class CapitalizadoPipe implements PipeTransform {    
+    transform(value: string, args:any[]): string {
+         return "hola mundo";   
+    }
+}
